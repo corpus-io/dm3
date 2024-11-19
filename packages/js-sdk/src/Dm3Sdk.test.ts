7ef92a35fa288849fe8475105d24feac0c746842
AlexNi245 <alex.plutta@googlemail.com>
2024-11-19 11:30:29 +0700

    use mock adapter to mock BE login flow

--- packages/js-sdk/src/Dm3Sdk.test.ts
+++ packages/js-sdk/src/Dm3Sdk.test.ts
@@ -1,34 +1,78 @@
+import { StorageAPI } from '@dm3-org/dm3-lib-storage';
+import {
+    MockedUserProfile,
+    mockUserProfile,
+} from '@dm3-org/dm3-lib-test-helper';
+import axios from 'axios';
 import { ethers } from 'ethers';
 import { Dm3Sdk, Dm3SdkConfig } from './Dm3Sdk';
-import { StorageAPI } from '@dm3-org/dm3-lib-storage';
+
+import MockAdapter from 'axios-mock-adapter';
+import { normalizeEnsName } from '@dm3-org/dm3-lib-profile';
 
 describe('Dm3Sdk', () => {
-    let upController: ethers.Signer;
+    let alice: MockedUserProfile;
+
+    //Axios mock to mock the http requests
+    let axiosMock;
 
     beforeEach(async () => {
-        upController = ethers.Wallet.createRandom();
+        alice = await mockUserProfile(
+            ethers.Wallet.createRandom(),
+            'alice.up',
+            ['test.io'],
+        );
     });
 
     it('test', async () => {
-        const luksoProvider = () => ({
-            send: () => Promise.resolve([]),
-            getSigner: () => Promise.resolve(upController),
-        });
+        axiosMock = new MockAdapter(axios);
+        //Mock BackendConnector HttpRequests
+        //Mock profileExistsOnDeliveryService
+        axiosMock
+            .onGet(
+                `http://localhost:4060/profile/${normalizeEnsName(
+                    alice.address,
+                )}.addr.test`,
+            )
+            .reply(200);
+
+        axiosMock
+            .onGet(
+                `http://localhost:4060/auth/${normalizeEnsName(
+                    alice.address,
+                )}.addr.test`,
+            )
+            .reply(200, 'mock-challenge');
+
+        axiosMock
+            .onPost(
+                `http://localhost:4060/auth/${normalizeEnsName(
+                    alice.address,
+                )}.addr.test`,
+            )
+            .reply(200, 'mock-challenge');
+
         const mockConfig: Dm3SdkConfig = {
             mainnetProvider: {} as ethers.providers.JsonRpcProvider,
-            lukso: luksoProvider as any,
+            storageApi: {} as StorageAPI,
             nonce: '1',
             defaultDeliveryService: 'test.io',
-            addressEnsSubdomain: 'addr.test',
-            userEnsSubdomain: 'user.test',
+            addressEnsSubdomain: '.addr.test',
+            userEnsSubdomain: '.user.test',
             resolverBackendUrl: 'resolver.io',
-            backendUrl: 'backend.io',
-            storageApi: {} as StorageAPI,
+            backendUrl: 'http://localhost:4060',
         };
 
-        // const dm3 = await new Dm3Sdk().universalProfileLogin();
-        // await dm3.conversations.addConversation('karl.eth');
-        // const c = dm3.conversations.list;
-        // const karl = c[0];
+        const dm3 = await new Dm3Sdk(mockConfig).login({
+            profileKeys: alice.profileKeys,
+            profile: alice.signedUserProfile,
+            accountAddress: alice.address,
+        });
+
+        console.log(dm3.conversations.conversations);
+
+        /*      await dm3.conversations.addConversation('karl.eth');
+        const c = dm3.conversations.conversations;
+        const karl = c[0]; */
     });
 });
